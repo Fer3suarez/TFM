f8ae6b8b2d47f5813700053bf77d00c67ac52138
Fernando Suárez <suarez11fer@gmail.com>
2022-05-08 16:53:26 +0200

    Trazas en el paquete de pruebas

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-describe("Market", function() {
+describe("** Paquete de pruebas del contrato inteligente Market.sol **", function() {
     it("Se crean dos NFTs y se realiza una compra y una venta", async function() {
       /* Despliegue del contrato inteligente */
       const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
@@ -11,15 +11,18 @@ describe("Market", function() {
       const auctionPrice = ethers.utils.parseUnits('1', 'ether')
   
       /* Creación de dos NFTs */
+      console.log(' - Creando dos NFTs')
       await nftMarket.createToken("https://www.mytokenlocation.com", auctionPrice, { value: precioGas })
       await nftMarket.createToken("https://www.mytokenlocation2.com", auctionPrice, { value: precioGas })
         
       const [_, buyerAddress] = await ethers.getSigners()
     
       /* Compra de un NFT */
+      console.log(' - Compra de un NFT con tokenID: 1')
       await nftMarket.connect(buyerAddress).compraNFT(1, { value: auctionPrice })
   
       /* Venta de un NFT */
+      console.log(' - Puesta en venta de un NFT con tokenID: 1')
       await nftMarket.connect(buyerAddress).venderNFT(1, auctionPrice, { value: precioGas })
   
       /* NFTs no vendidos */
@@ -35,6 +38,6 @@ describe("Market", function() {
         }
         return item
       }))
-      console.log('items: ', items)
+      console.log(' - NFTs puestos en venta: ', items)
     })
   })
